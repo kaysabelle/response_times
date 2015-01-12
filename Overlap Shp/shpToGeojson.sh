039e6d7e633c9d1b250283be1cfaf54ad737c4e0
kaysabelle <kaysabelle@gmail.com>
2015-01-12 14:09:49 -0800

    Removed ToBreak flag
    No longer breaking up response areas by minute. For the initial phase, we just want to map the total response areas so we will have less files to deal with.

--- Overlap Shp/shpToGeojson.sh
+++ Overlap Shp/shpToGeojson.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ogr2ogr -f "GeoJson" -t_srs EPSG:4326 -sql "SELECT * FROM 'ResponseAreasOverlap20150112' 
-# WHERE "Name" LIKE 'Mesa%' AND 'ToBreak' = 8" MesaTo8.geojson ResponseAreasOverlap20150112.shp
+# WHERE "Name" LIKE 'Mesa%' Mesa.geojson ResponseAreasOverlap20150112.shp
 
 while read line
 do
