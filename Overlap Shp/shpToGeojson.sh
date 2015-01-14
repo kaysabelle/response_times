0886d0a102fa5cc81dd7eed9d298f1eb14128c48
Brian Yu <brianyu1125@gmail.com>
2015-01-14 10:02:55 -0800

    Finished script and have all geojson files

--- Overlap Shp/shpToGeojson.sh
+++ Overlap Shp/shpToGeojson.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
 
+# what command to use with sql statement
 # ogr2ogr -f "GeoJson" -t_srs EPSG:4326 -sql "SELECT * FROM 'ResponseAreasOverlap20150112' 
-# WHERE "Name" LIKE 'Mesa%' Mesa.geojson ResponseAreasOverlap20150112.shp
+# WHERE "Name" LIKE 'Mesa%' AND 'ToBreak' = 8" MesaTo8.geojson ResponseAreasOverlap20150112.shp
+
+# Initialize array breakPoints
+breakPoints=(7 8 15 20 30)
+
+for i in *.shp; do
+	db=${i%.*}
+	echo $db
+done
 
 while read line
 do
     name=$line
-    echo "Text read from file - $name"
+    # takes out all whitespace in name for output file name
+    output=`echo $name | tr -d ' '`
 
-	for i in *.shp; do
-		echo db=${i%.*}
+    # creates geojson containing all times of that station
+    ogr2ogr -f "GeoJson" -t_srs EPSG:4326 \
+    -sql "SELECT * FROM '${db}' WHERE "Name" LIKE '${name}%'" \
+    ${output}.geojson ${db}.shp
 
-	done
+    # for every break point, create that geojson of that station
+	# for point in "${breakPoints[@]}"; do
+	# 	# Create output file name
+	# 	outputFile="${output}"To"${point}".geojson
 
-done < $1
+	# 	# creates geojson of all different break points
+	# 	ogr2ogr -f "GeoJson" -t_srs EPSG:4326 \
+	# 	-sql "SELECT * FROM '${db}' WHERE "Name" LIKE '${name}%' AND 'ToBreak' = ${point}" \
+	#     ${outputFile} ${db}.shp
+	# done
+
+done < FSList.txt
